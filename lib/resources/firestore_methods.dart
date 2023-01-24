6920ad7a443a73ac91329bf18d21aa3877a37106
yavuzaydn <yavuz5680@gmail.com>
2023-01-25 00:53:25 +0300

    first commit

---
+++ lib/resources/firestore_methods.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FireStoreMethods {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Stream get meetingsHistory => _firestore
+          .collection('users')
+          .doc(_auth.currentUser!.uid)
+          .collection('meetings').snapshots();
+
+  void addToMeetingHistory(String meetingName) async {
+    try {
+      await _firestore
+          .collection('users')
+          .doc(_auth.currentUser!.uid)
+          .collection('meetings')
+          .add({
+        'meetingName': meetingName,
+        'cratedAt': DateTime.now(),
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+}
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:zoom_clone/resources/auth_methods.dart';
+import 'package:zoom_clone/widgets/custom_button.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final AuthMethods _authMethods =AuthMethods();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "Start or join a meeting",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 38),
+            child: Image.asset(
+                '/Users/yavuzaydin/zoom_clone/assets/images/onboarding.jpg'),
+          ),
+          CustomButton(
+            text: 'Google Sign In',
+            onPressed: () async{
+              bool res = await _authMethods.signInWithGoogle(context);
+              if(res){
+                Navigator.pushNamed(context, '/home');
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
